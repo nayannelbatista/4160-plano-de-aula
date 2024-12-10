7bd2a48f9f404243fa009ee43180d2d38a97b4fc
nayannelbatista <nayannelopes.bd@gmail.com>
2024-12-10 11:29:16 -0300

    5.2 - inc. hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(), 
-    provideClientHydration()
+    provideClientHydration(withIncrementalHydration())
   ]
 };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,12 @@ import { Meta, Title } from '@angular/platform-browser';
 import { ProductsListComponent } from '../../components/products-list/products-list.component';
 
 @Component({
-    selector: 'app-home',
-    imports: [ProductsListComponent],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+  selector: 'app-home',
+  imports: [ProductsListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   private readonly title = inject(Title);
   private readonly meta = inject(Meta);
@@ -33,11 +33,11 @@ export class HomeComponent implements OnInit{
   setPageMeta() {
     this.title.setTitle('Deleite - a melhor experiência em sabores');
     this.meta.addTags([
-      { name: 'description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!'},
-      { property: 'og:title', content: 'Deleite - a melhor experiência em sabores'},
-      { property: 'og:description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!'},
-      { property: 'og:image', content: 'assets/images/logo.png'},
-      { name: 'twitter:card', content: 'summary_large_image'}
+      { name: 'description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!' },
+      { property: 'og:title', content: 'Deleite - a melhor experiência em sabores' },
+      { property: 'og:description', content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite. Sabor e qualidade em cada produto!' },
+      { property: 'og:image', content: 'assets/images/logo.png' },
+      { name: 'twitter:card', content: 'summary_large_image' }
     ])
   }
 }
